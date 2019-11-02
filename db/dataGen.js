939601fdee7efae472d8b5b4832330b44315749e
nick.chand.20@gmail.com <Nick@Nicholass-MacBook-Pro.local>
2019-11-01 21:36:54 -0700

    Remove reference function and reference csv file

--- db/dataGen.js
+++ db/dataGen.js
@@ -51,38 +51,3 @@ writeOneThousandListings(writeListings, 'utf-8', () => {
 });
 
 //unique commentId can be attributed to i;
-
-const writeUsers = fs.createWriteStream('users.csv');
-writeUsers.write('id,username,avatar\n', 'utf8');
-
-function writeTenThousandUsers(writer, encoding, callback) {
-  let i = 10000;
-  let id = 0;
-  function write() {
-    let ok = true;
-    do {
-      i -= 1;
-      id += 1;
-      const username = faker.internet.userName();
-      const avatar = faker.image.avatar();
-      const data = `${id},${username},${avatar}\n`;
-      if (i === 0) {
-        writer.write(data, encoding, callback);
-      } else {
-        // see if we should continue, or wait
-        // don't pass the callback, because we're not done yet.
-        ok = writer.write(data, encoding);
-      }
-    } while (i > 0 && ok);
-    if (i > 0) {
-      // had to stop early!
-      // write some more once it drains
-      writer.once('drain', write);
-    }
-  }
-  write();
-}
-
-writeTenThousandUsers(writeUsers, 'utf-8', () => {
-  writeUsers.end();
-});
